dd45e52c5da78863efa34b3d492883fe880a480f
Sandesh <noobielegends07@gmail.com>
2021-11-01 19:05:54 +0530

    emailtemp1

--- delete_campaign.php
+++ delete_campaign.php
@@ -6,42 +6,142 @@ require_once('wp-config.php');
 global $wpdb;
 $campaign_id = $_POST['campaign_id'];
 
-$campaigns = $wpdb->get_row("SELECT * FROM wp_campaigns WHERE id = '".$campaign_id."'");
-$service_name = $services->service_name;
-$user_id = $services->userId;
-$start_date = date("d-m-Y", strtotime($services->start_date));
-$end_date = date("d-m-Y", strtotime($services->end_date));
-$description = $services->description;
-$link = BASE_URL.'my-services';
+$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}campaigns WHERE id =" . $campaign_id, OBJECT);
+$res = $results[0];
+if ($res->campaign_typeId == 2) {
+    $fundtitle = $res->item_name;
+    $goal_amount = $res->item_qty;
+    $currency = 'QTY';
+} else if ($res->campaign_typeId == 3) {
+    $fundtitle = $res->product_name;
+    $goal_amount = $res->product_price * $res->product_qty;
+    $currency = $res->currency;
+} else {
+    $fundtitle = $res->fundraiser_title;
+    $goal_amount = $res->goal_amount;
+    $currency = $res->currency;
+}
 
-$users = get_user_by('id',$user_id);
-$user_email = $users->user_email;
-$user_nicename = $users->user_nicename;
+$campTypeId = $res->campaign_typeId;
+$targetDate = $res->end_date;
+
+$resultsdonacc = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}campaigndonations WHERE status = 1 AND campaign_Id IN(" . $campaign_id . ")", ARRAY_A);
+$achieve_amount = 0;
+foreach ($resultsdonacc as $tt) {
+    if ($tt['campaign_typeId'] == 3) {
+        $achieve_amountn = $tt['amount'] ? $tt['amount'] * ($res->product_price ? $res->product_price : 0) : 0;
+    } else {
+        $achieve_amountn = $tt['amount'] ? $tt['amount'] : 0;
+    }
+    $achieve_amount += $achieve_amountn;
+}
+
+if ($res->image) {
+    $iimage = BASE_URL . 'fundraiserimg/' . $res->image;
+} else {
+    $iimagei = str_replace("https://www.youtube.com/watch?v=", "", $res->video);
+    $iimage = "https://img.youtube.com/vi/" . $iimagei . "/maxresdefault.jpg";
+}
+$fundraiser_title = $campaigns->fundraiser_title;
+$item_name = $campaigns->item_name;
+$product_name = $campaigns->product_name;
+$product_name = $campaigns->campaign_typeId;
+if ($campaign_typeId == 2) {
+    $fundtitle = $item_name;
+} else if ($campaign_typeId == 3) {
+    $fundtitle = $product_name;
+} else {
+    $fundtitle = $fundraiser_title;
+}
+$userId = $res->userId;
+$resultsusers = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}users WHERE id = " . $userId, OBJECT);
+$user_email = $resultsusers[0]->user_email;
+$display_name = $resultsusers[0]->display_name;
+$all_meta_for_user = get_user_meta($resultsusers[0]->ID);
+$phone = $all_meta_for_user['billing_phone'][0];
+
+$resultsc = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}campaigntypes WHERE id =" . $res->campaign_typeId, OBJECT);
+$res = $results[0];
+$campt = $resultsc[0]->title;
+
+$detaillink = BASE_URL . "campaign-detail/?id=" . $campaign_id;
+
+$subjectnn = "ZED$campaign_id - $campt - $fundtitle - $res->address - Has been  by ZedAid, you can track the progress at : $detaillink";
+
+$to = $user_email;
+$subject = "ZED$campaign_id - $campt - $fundtitle - $address - Has been deleted";
+$from = 'info@zedaid.org';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Create email headers
+$headers .= 'From: ZED Foundation <' . $from . '>'."\r\n" .
+    'CC: info@zedaid.org'. "\r\n" . 
+    'Reply-To: ' . $from . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
+
+$file = 'email/camp_deleted.html';    
+$myfile = fopen($file, "r") or die("Unable to open file!");
+$message2 = fread($myfile,filesize($file));
+
+$message2 = str_replace('{{Name}}', $display_name , $message2);
+$message2 = str_replace('{{campaign Name}}', $fundtitle , $message2);
+$message2 = str_replace('{{title}}', $fundtitle , $message2);
+$message2 = str_replace('{{UNIT}}', $currency , $message2);
+$message2 = str_replace('{{AMOUNT}}', number_format($goal_amount) , $message2);
+$message2 = str_replace('{{TARGET_DATE}}', date("d M Y", strtotime($targetDate)) , $message2);
+
+// // Compose a simple HTML email message
+// $message = '<html><body>';
+// $message .= '<h3>Hello, ' . $display_name . '</h3>';
+// $message .= '<p style="font-size:18px;">Your campaign <b>' . $fundtitle . '</b> has beed approved by admin.</p>';
+// $message .= '<h4>Campaign Details:</h4>';
+// $message .= '<p><b>Campaign Title:</b>' . $fundtitle . '</p>';
+// $message .= '<div style="background: url(' . $iimage . ') center center;background-size: cover;"></div>';
+// $message .= html_entity_decode($res->short_description);
+// $message .= '<p><b>Goal Amount:</b>' . $currency . ' ' . $goal_amount.'</p>';
+// $message .= '<p><b>Achived Amount:</b>' . $currency . ' ' . $achieve_amount.'</p>';
+// $message .= '</body></html>';
+
+wp_mail($to, $subject, $message2, $headers);
+
+$smstext = 'Dear '.$display_name.', your campaign is approved by ZedAid. Start sharing the campaign with donors. '.$detaillink;
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.msg91.com/api/v2/sendsms",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{\"sender\":\"JEDAII\",\"route\":\"4\",\"country\":\"91\",\"DLT_TE_ID\":\"1307162620980005462\",\"sms\":[{\"message\":\"$smstext\",\"to\":[\"$phone\"]}]}",
+    CURLOPT_HTTPHEADER => array(
+        "authkey: 328268AKM9eIBEQ5eb00746P1",
+        "cache-control: no-cache",
+        "content-type: application/json",
+        "postman-token: 976efc79-51b6-d3db-3727-e4173cb180f4"
+    ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    // echo "cURL Error #:" . $err;
+} else {
+    // echo $response;
+}
+
+
+
 
 $wpdb->query($wpdb->prepare("UPDATE wp_campaigns SET `status` = '2',`admin_approved` = '0',`zed_verified`='0' WHERE id=$campaign_id"));
-
-$to0 = $user_email;
-$subject0 =  "ZedAid - Service is deleted";
-$from0 = 'info@zedaid.org';
-// To send HTML mail, the Content-type header must be set
-$headers0  = 'MIME-Version: 1.0' . "\r\n";
-$headers0 .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-// Create email headers
-$headers0 .= 'From: ZED Foundation <' . $from0 . '>'."\r\n" .
-    'CC: info@zedaid.org'. "\r\n" . 
-    'Reply-To: ' . $from0 . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-$file = 'email/service_request_admin.html';    
-$myfile = fopen($file, "r") or die("Unable to open file!");
-$message0 = fread($myfile,filesize($file));
-$message0 = str_replace('{{NAME}}', $user_nicename , $message0);
-$message0 = str_replace('{{SERVICE_NAME}}', $service_name , $message0);
-$message0 = str_replace('{{START_DATE}}', $start_date , $message0);
-$message0 = str_replace('{{END_DATE}}', $end_date , $message0);
-$message0 = str_replace('{{DESCRIPTION}}', $description , $message0);
-$message0 = str_replace('{{LINK}}', $link , $message0);
-wp_mail($to0, $subject0, $message0, $headers0);
-
 echo 'true';
 exit;
